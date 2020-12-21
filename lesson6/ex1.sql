494e0f398dbb5e026b97da6d60dce46cd405511e
Krivosheenkova <71779610+Krivosheenkova@users.noreply.github.com>
2020-12-21 18:29:32 +0300

    Update ex1.sql

--- lesson6/ex1.sql
+++ lesson6/ex1.sql
@@ -5,8 +5,8 @@ WHERE `to_users_id` = 101
 	AND `from_users_id` IN (
 		SELECT `from_users_id` 
 		FROM `my_vk_schema`.`friend_requests` 
-		WHERE (`to_users_id` = 101) AND STATUS = 1)
-	AND (SELECT COUNT(*) AS `count` FROM `my_vk_schema`.`messages` WHERE `to_users_id` = 101 GROUP BY `from_users_id` ORDER BY `count` DESC LIMIT 1)
+		WHERE (`to_users_id` = `to_users_id`) AND STATUS = 1)
+	AND (SELECT COUNT(*) AS `count` FROM `my_vk_schema`.`messages` WHERE `to_users_id` = `to_users_id` GROUP BY `from_users_id` ORDER BY `count` DESC LIMIT 1)
   GROUP BY `from_users_id` ORDER BY COUNT(*) DESC LIMIT 1;
 +---------------+
 | from_users_id |
