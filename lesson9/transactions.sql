9496b48f33996062a951cae1b87f0c6aec0b6f3d
Krivosheenkova <71779610+Krivosheenkova@users.noreply.github.com>
2021-01-18 16:07:23 +0300

    Update transactions.sql

--- lesson9/transactions.sql
+++ lesson9/transactions.sql
@@ -64,3 +64,59 @@ SELECT * FROM prod_cat;
 |          7 | MSI B250M GAMING PRO    | Материнские платы                 |
 +------------+-------------------------+-----------------------------------+
 7 rows in set (0.01 sec)
+
+"""
+3. (по желанию) Пусть имеется таблица с календарным полем created_at. 
+В ней размещены разряженые календарные записи за август 2018 года '2018-08-01', '2016-08-04', '2018-08-16' и 2018-08-17. 
+Составьте запрос, который выводит полный список дат за август, выставляя в соседнем поле значение 1, 
+если дата присутствует в исходном таблице и 0, если она отсутствует.
+"""
+SELECT 
+	period.selected_date AS day,
+	(SELECT EXISTS(SELECT * FROM vk.profiles WHERE DATE(created_at) = day)) AS is_in_tbl
+FROM
+	(SELECT v.* FROM 
+		(SELECT ADDDATE('2020-01-01',t4.i*10000 + t3.i*1000 + t2.i*100 + t1.i*10 + t0.i) selected_date FROM
+			(SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t0,
+		    (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t1,
+		    (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t2,
+		    (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t3,
+		    (SELECT 0 i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) t4) v
+WHERE selected_date BETWEEN '2020-08-01' AND '2020-08-31') AS period ORDER BY day;
++------------+-----------+
+| day        | is_in_tbl |
++------------+-----------+
+| 2020-08-01 |         1 |
+| 2020-08-02 |         0 |
+| 2020-08-03 |         0 |
+| 2020-08-04 |         0 |
+| 2020-08-05 |         0 |
+| 2020-08-06 |         0 |
+| 2020-08-07 |         0 |
+| 2020-08-08 |         0 |
+| 2020-08-09 |         0 |
+| 2020-08-10 |         1 |
+| 2020-08-11 |         0 |
+| 2020-08-12 |         0 |
+| 2020-08-13 |         0 |
+| 2020-08-14 |         0 |
+| 2020-08-15 |         0 |
+| 2020-08-16 |         0 |
+| 2020-08-17 |         0 |
+| 2020-08-18 |         0 |
+| 2020-08-19 |         0 |
+| 2020-08-20 |         0 |
+| 2020-08-21 |         1 |
+| 2020-08-22 |         0 |
+| 2020-08-23 |         0 |
+| 2020-08-24 |         0 |
+| 2020-08-25 |         0 |
+| 2020-08-26 |         1 |
+| 2020-08-27 |         0 |
+| 2020-08-28 |         0 |
+| 2020-08-29 |         0 |
+| 2020-08-30 |         1 |
+| 2020-08-31 |         0 |
++------------+-----------+
+31 rows in set (0.17 sec)
+
